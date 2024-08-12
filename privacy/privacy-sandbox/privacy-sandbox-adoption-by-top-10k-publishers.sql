f538ecbd81587c071dc43686a43115153673d8a9
Yash Vekaria <30694521+Yash-Vekaria@users.noreply.github.com>
2024-08-11 17:58:19 -0700

    Update privacy-sandbox-adoption-by-top-10k-publishers.sql

--- privacy/privacy-sandbox/privacy-sandbox-adoption-by-top-10k-publishers.sql
+++ privacy/privacy-sandbox/privacy-sandbox-adoption-by-top-10k-publishers.sql
@@ -1,118 +1,171 @@
 -- Usage of different Privacy Sandbox (PS) features by Top 10K publishers
 -- Example: publisher.com: {“runAdAuction”: 10} [“runAdAuction” is a PS feature column and 10 is #TPs calling “runAdAuction” on publisher.com]
--- This query will process 63.29 TB when run.
-WITH pages AS (
+
+-- Extracting third-parties observed using PS APIs on a publisher
+CREATE TEMP FUNCTION jsonObjectKeys(input STRING)
+RETURNS ARRAY<STRING>
+LANGUAGE js AS """
+  if (!input) {
+    return [];
+  }
+  return Object.keys(JSON.parse(input));
+""";
+
+-- Extracting PS APIs being called by a given third-party (passed as "key")
+CREATE TEMP FUNCTION jsonObjectValues(input STRING, key STRING)
+RETURNS ARRAY<STRING>
+LANGUAGE js AS """
+  if (!input) {
+    return [];
+  }
+  const jsonObject = JSON.parse(input);
+  const values = jsonObject[key] || [];
+  
+  function splitByDelimiters(value) {
+    const delimiterRegex = new RegExp(',|, |\\n|\\u0000', 'g');
+    return value.split(delimiterRegex).map(v => v.trim()).filter(v => v);
+  }
+
+  const result = [];
+  values.forEach(value => {
+    if (value.startsWith('accept-ch|')) {
+      const parts = splitByDelimiters(value.replace('accept-ch|', ''));
+      parts.forEach(part => {
+        if (["UA", "Arch", "Bitness", "Full-Version-List", "Mobile", "Model", "Platform", "Platform-Version", "WoW64"].includes(part)) {
+          result.push("Sec-CH-UA-" + part);
+        } else {
+          let formattedPart = part.split('-').map(segment => 
+            segment.charAt(0).toUpperCase() + segment.slice(1).toLowerCase()
+          ).join('-');
+          formattedPart = formattedPart.replace('Ch', 'CH');
+          formattedPart = formattedPart.replace('Ua', 'UA');
+          formattedPart = formattedPart.replace('Wow64', 'WoW64');
+          formattedPart = formattedPart.replace('Dpr', 'DPR');
+          formattedPart = formattedPart.replace('Rtt', 'RTT');
+          formattedPart = formattedPart.replace('Ect', 'ECT');
+          result.push(formattedPart);
+        }
+      });
+    } else {
+      result.push(value);
+    }
+  });
+
+  return result;
+""";
+
+WITH privacy_sandbox_features AS (
   SELECT
     NET.REG_DOMAIN(page) AS publisher,
-    JSON_EXTRACT(custom_metrics, '$._privacy-sandbox.privacySandBoxAPIUsage') AS api_usage
-  FROM `httparchive.all.pages`
-  WHERE
-    date = '2024-06-01' AND
-    client = 'desktop' AND
-    is_root_page = TRUE AND
-    rank <= 10000
-),
-exploded_features AS (
-  SELECT
-    publisher,
-    REGEXP_EXTRACT(feature, r'"([^"]+)":') AS third_party_domain,
-    TRIM(value) AS feature_values
-  FROM
-    pages,
-    UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(api_usage), r'"([^"]+)":\s*\[([^\]]*)\]')) AS feature,
-    UNNEST(SPLIT(REGEXP_EXTRACT(feature, r'\[([^\]]*)\]'), ',')) AS value
-),
-cleaned_features AS (
-  SELECT
-    publisher,
     third_party_domain,
     CASE
-      WHEN feature_values LIKE 'accept-ch|%' THEN
-        SPLIT(SUBSTR(feature_values, STRPOS(feature_values, '|') + 1), ',')
-      WHEN feature_values LIKE '%opics|%' THEN
-        [REPLACE(SUBSTR(feature_values, 0, STRPOS(feature_values, '|') - 1) || '-' || SPLIT(feature_values, '|')[SAFE_OFFSET(1)], '|', '-')]
-      WHEN feature_values LIKE 'attribution-reporting-register-source%' THEN
-        [SPLIT(feature_values, '|')[OFFSET(0)]]
+      WHEN api LIKE '%opics%|%' THEN
+        REPLACE(SUBSTR(api, 0, STRPOS(api, '|') - 1) || '-' || SPLIT(api, '|')[SAFE_OFFSET(1)], '|', '-')
+      WHEN api LIKE 'attribution-reporting-register-source%' THEN
+        SPLIT(api, '|')[OFFSET(0)]
       ELSE
-        [feature_values]
-    END AS features
-  FROM exploded_features
+        api
+    END AS feature
+  FROM `httparchive.all.pages`,
+  UNNEST(jsonObjectKeys(JSON_QUERY(custom_metrics, '$.privacy-sandbox.privacySandBoxAPIUsage'))) AS third_party_domain,
+  UNNEST(jsonObjectValues(JSON_QUERY(custom_metrics, '$.privacy-sandbox.privacySandBoxAPIUsage'), third_party_domain)) AS api
+  WHERE
+      date = '2024-06-01' AND
+      client = 'desktop' AND
+      is_root_page = TRUE AND
+      rank <= 10000
 ),
-distinct_features AS (
+
+grouped_features AS (
   SELECT
     publisher,
     feature,
-    COUNT(DISTINCT third_party_domain) AS tp_count
-  FROM cleaned_features,
-  UNNEST(features) AS feature
+    COUNT(DISTINCT third_party_domain) AS third_party_count
+  FROM privacy_sandbox_features
   GROUP BY publisher, feature
-), 
-pivoted_data AS (
-  SELECT
-    publisher,
-    MAX(IF(feature = 'runAdAuction', tp_count, 0)) AS runAdAuction,
-    MAX(IF(feature = 'navigator.userAgentData.getHighEntropyValues', tp_count, 0)) AS navigator_userAgentData_getHighEntropyValues,
-    MAX(IF(feature = 'fencedFrameJs', tp_count, 0)) AS fencedFrameJs,
-    MAX(IF(feature = 'attribution-reporting-eligible', tp_count, 0)) AS attribution_reporting_eligible,
-    MAX(IF(feature = 'attribution-reporting-register-source', tp_count, 0)) AS attribution_reporting_register_source,
-    MAX(IF(feature = 'navigator.credentials.get', tp_count, 0)) AS navigator_credentials_get,
-    MAX(IF(feature = 'IdentityProvider.getUserInfo', tp_count, 0)) AS IdentityProvider_getUserInfo,
-    MAX(IF(feature = 'IdentityProvider.close', tp_count, 0)) AS IdentityProvider_close,
-    MAX(IF(feature = 'navigator.login.setStatus', tp_count, 0)) AS navigator_login_setStatus,
-    MAX(IF(feature = 'fencedFrameJs', tp_count, 0)) AS fencedFrameJs,
-    MAX(IF(feature = 'FencedFrameConfig.setSharedStorageContext', tp_count, 0)) AS FencedFrameConfig_setSharedStorageContext,
-    MAX(IF(feature = 'window.fence.getNestedConfigs', tp_count, 0)) AS window_fence_getNestedConfigs,
-    MAX(IF(feature = 'window.fence.reportEvent', tp_count, 0)) AS window_fence_reportEvent,
-    MAX(IF(feature = 'window.fence.setReportEventDataForAutomaticBeacons', tp_count, 0)) AS window_fence_setReportEventDataForAutomaticBeacons,
-    MAX(IF(feature = 'fenced-frame', tp_count, 0)) AS fenced_frame,
-    MAX(IF(feature = 'document.interestCohort', tp_count, 0)) AS document_interestCohort,
-    MAX(IF(feature = 'privateAggregation.contributeToHistogram', tp_count, 0)) AS privateAggregation_contributeToHistogram,
-    MAX(IF(feature = 'privateAggregation.contributeToHistogramOnEvent', tp_count, 0)) AS privateAggregation_contributeToHistogramOnEvent,
-    MAX(IF(feature = 'privateAggregation.enableDebugMode', tp_count, 0)) AS privateAggregation_enableDebugMode,
-    MAX(IF(feature = 'document.hasPrivateToken', tp_count, 0)) AS document_hasPrivateToken,
-    MAX(IF(feature = 'document.hasRedemptionRecord', tp_count, 0)) AS document_hasRedemptionRecord,
-    MAX(IF(feature = 'sec-private-state-token', tp_count, 0)) AS sec_private_state_token,
-    MAX(IF(feature = 'sec-redemption-record', tp_count, 0)) AS sec_redemption_record,
-    MAX(IF(feature = 'joinAdInterestGroup', tp_count, 0)) AS joinAdInterestGroup,
-    MAX(IF(feature = 'leaveAdInterestGroup', tp_count, 0)) AS leaveAdInterestGroup,
-    MAX(IF(feature = 'updateAdInterestGroups', tp_count, 0)) AS updateAdInterestGroups,
-    MAX(IF(feature = 'clearOriginJoinedAdInterestGroups', tp_count, 0)) AS clearOriginJoinedAdInterestGroups,
-    MAX(IF(feature = 'runAdAuction', tp_count, 0)) AS runAdAuction,
-    MAX(IF(feature = 'generateBid', tp_count, 0)) AS generateBid,
-    MAX(IF(feature = 'scoreAd', tp_count, 0)) AS scoreAd,
-    MAX(IF(feature = 'reportWin', tp_count, 0)) AS reportWin,
-    MAX(IF(feature = 'reportResult', tp_count, 0)) AS reportResult,
-    MAX(IF(feature = 'window.sharedStorage.append', tp_count, 0)) AS window_sharedStorage_append,
-    MAX(IF(feature = 'window.sharedStorage.clear', tp_count, 0)) AS window_sharedStorage_clear,
-    MAX(IF(feature = 'window.sharedStorage.delete', tp_count, 0)) AS window_sharedStorage_delete,
-    MAX(IF(feature = 'window.sharedStorage.set', tp_count, 0)) AS window_sharedStorage_set,
-    MAX(IF(feature = 'window.sharedStorage.run', tp_count, 0)) AS window_sharedStorage_run,
-    MAX(IF(feature = 'window.sharedStorage.selectURL', tp_count, 0)) AS window_sharedStorage_selectURL,
-    MAX(IF(feature = 'window.sharedStorage.worklet.addModule', tp_count, 0)) AS window_sharedStorage_worklet_addModule,
-    MAX(IF(feature = 'document.hasStorageAccess', tp_count, 0)) AS document_hasStorageAccess,
-    MAX(IF(feature = 'document.hasUnpartitionedCookieAccess', tp_count, 0)) AS document_hasUnpartitionedCookieAccess,
-    MAX(IF(feature = 'document.requestStorageAccess', tp_count, 0)) AS document_requestStorageAccess,
-    MAX(IF(feature = 'document.requestStorageAccessFor', tp_count, 0)) AS document_requestStorageAccessFor,
-    MAX(IF(feature = 'document.browsingTopics-false', tp_count, 0)) AS document_browsingTopics_false,
-    MAX(IF(feature = 'document.browsingTopics-true', tp_count, 0)) AS document_browsingTopics_true,
-    MAX(IF(feature = 'sec-browsing-topics-false', tp_count, 0)) AS sec_browsing_topics_false,
-    MAX(IF(feature = 'sec-browsing-topics-true', tp_count, 0)) AS sec_browsing_topics_true,
-    MAX(IF(feature = 'navigator.userAgentData.getHighEntropyValues', tp_count, 0)) AS navigator_userAgentData_getHighEntropyValues,
-    MAX(IF(feature = 'Sec-CH-UA', tp_count, 0)) AS Sec_CH_UA,
-    MAX(IF(feature = 'Sec-CH-UA-Arch', tp_count, 0)) AS Sec_CH_UA_Arch,
-    MAX(IF(feature = 'Sec-CH-UA-Bitness', tp_count, 0)) AS Sec_CH_UA_Bitness,
-    MAX(IF(feature = 'Sec-CH-UA-Full-Version', tp_count, 0)) AS Sec_CH_UA_Full_Version,
-    MAX(IF(feature = 'Sec-CH-UA-Full-Version-List', tp_count, 0)) AS Sec_CH_UA_Full_Version_List,
-    MAX(IF(feature = 'Sec-CH-UA-Mobile', tp_count, 0)) AS Sec_CH_UA_Mobile,
-    MAX(IF(feature = 'Sec-CH-UA-Model', tp_count, 0)) AS Sec_CH_UA_Model,
-    MAX(IF(feature = 'Sec-CH-UA-Platform', tp_count, 0)) AS Sec_CH_UA_Platform,
-    MAX(IF(feature = 'Sec-CH-UA-Platform-Version', tp_count, 0)) AS Sec_CH_UA_Platform_Version,
-    MAX(IF(feature = 'Sec-CH-UA-WoW64', tp_count, 0)) AS Sec_CH_UA_WoW64
-  FROM distinct_features
-  GROUP BY publisher
 )
-
-SELECT *
-FROM pivoted_data
-ORDER BY publisher
-LIMIT 10000;
+SELECT
+  publisher,
+  SUM(IF(feature = 'runAdAuction', third_party_count, 0)) AS runAdAuction,
+  SUM(IF(feature = 'navigator.userAgentData.getHighEntropyValues', third_party_count, 0)) AS navigator_userAgentData_getHighEntropyValues,
+  SUM(IF(feature = 'fencedFrameJs', third_party_count, 0)) AS fencedFrameJs,
+  SUM(IF(feature = 'attribution-reporting-eligible', third_party_count, 0)) AS attribution_reporting_eligible,
+  SUM(IF(feature = 'attribution-reporting-register-source', third_party_count, 0)) AS attribution_reporting_register_source,
+  SUM(IF(feature = 'navigator.credentials.get', third_party_count, 0)) AS navigator_credentials_get,
+  SUM(IF(feature = 'IdentityProvider.getUserInfo', third_party_count, 0)) AS IdentityProvider_getUserInfo,
+  SUM(IF(feature = 'IdentityProvider.close', third_party_count, 0)) AS IdentityProvider_close,
+  SUM(IF(feature = 'navigator.login.setStatus', third_party_count, 0)) AS navigator_login_setStatus,
+  SUM(IF(feature = 'fencedFrameJs', third_party_count, 0)) AS fencedFrameJs,
+  SUM(IF(feature = 'FencedFrameConfig.setSharedStorageContext', third_party_count, 0)) AS FencedFrameConfig_setSharedStorageContext,
+  SUM(IF(feature = 'window.fence.getNestedConfigs', third_party_count, 0)) AS window_fence_getNestedConfigs,
+  SUM(IF(feature = 'window.fence.reportEvent', third_party_count, 0)) AS window_fence_reportEvent,
+  SUM(IF(feature = 'window.fence.setReportEventDataForAutomaticBeacons', third_party_count, 0)) AS window_fence_setReportEventDataForAutomaticBeacons,
+  SUM(IF(feature = 'fenced-frame', third_party_count, 0)) AS fenced_frame,
+  SUM(IF(feature = 'document.interestCohort', third_party_count, 0)) AS document_interestCohort,
+  SUM(IF(feature = 'privateAggregation.contributeToHistogram', third_party_count, 0)) AS privateAggregation_contributeToHistogram,
+  SUM(IF(feature = 'privateAggregation.contributeToHistogramOnEvent', third_party_count, 0)) AS privateAggregation_contributeToHistogramOnEvent,
+  SUM(IF(feature = 'privateAggregation.enableDebugMode', third_party_count, 0)) AS privateAggregation_enableDebugMode,
+  SUM(IF(feature = 'document.hasPrivateToken', third_party_count, 0)) AS document_hasPrivateToken,
+  SUM(IF(feature = 'document.hasRedemptionRecord', third_party_count, 0)) AS document_hasRedemptionRecord,
+  SUM(IF(feature = 'sec-private-state-token', third_party_count, 0)) AS sec_private_state_token,
+  SUM(IF(feature = 'sec-redemption-record', third_party_count, 0)) AS sec_redemption_record,
+  SUM(IF(feature = 'joinAdInterestGroup', third_party_count, 0)) AS joinAdInterestGroup,
+  SUM(IF(feature = 'leaveAdInterestGroup', third_party_count, 0)) AS leaveAdInterestGroup,
+  SUM(IF(feature = 'updateAdInterestGroups', third_party_count, 0)) AS updateAdInterestGroups,
+  SUM(IF(feature = 'clearOriginJoinedAdInterestGroups', third_party_count, 0)) AS clearOriginJoinedAdInterestGroups,
+  SUM(IF(feature = 'runAdAuction', third_party_count, 0)) AS runAdAuction,
+  SUM(IF(feature = 'generateBid', third_party_count, 0)) AS generateBid,
+  SUM(IF(feature = 'scoreAd', third_party_count, 0)) AS scoreAd,
+  SUM(IF(feature = 'reportWin', third_party_count, 0)) AS reportWin,
+  SUM(IF(feature = 'reportResult', third_party_count, 0)) AS reportResult,
+  SUM(IF(feature = 'window.sharedStorage.append', third_party_count, 0)) AS window_sharedStorage_append,
+  SUM(IF(feature = 'window.sharedStorage.clear', third_party_count, 0)) AS window_sharedStorage_clear,
+  SUM(IF(feature = 'window.sharedStorage.delete', third_party_count, 0)) AS window_sharedStorage_delete,
+  SUM(IF(feature = 'window.sharedStorage.set', third_party_count, 0)) AS window_sharedStorage_set,
+  SUM(IF(feature = 'window.sharedStorage.run', third_party_count, 0)) AS window_sharedStorage_run,
+  SUM(IF(feature = 'window.sharedStorage.selectURL', third_party_count, 0)) AS window_sharedStorage_selectURL,
+  SUM(IF(feature = 'window.sharedStorage.worklet.addModule', third_party_count, 0)) AS window_sharedStorage_worklet_addModule,
+  SUM(IF(feature = 'document.hasStorageAccess', third_party_count, 0)) AS document_hasStorageAccess,
+  SUM(IF(feature = 'document.hasUnpartitionedCookieAccess', third_party_count, 0)) AS document_hasUnpartitionedCookieAccess,
+  SUM(IF(feature = 'document.requestStorageAccess', third_party_count, 0)) AS document_requestStorageAccess,
+  SUM(IF(feature = 'document.requestStorageAccessFor', third_party_count, 0)) AS document_requestStorageAccessFor,
+  SUM(IF(feature = 'document.browsingTopics-false', third_party_count, 0)) AS document_browsingTopics_false,
+  SUM(IF(feature = 'document.browsingTopics-true', third_party_count, 0)) AS document_browsingTopics_true,
+  SUM(IF(feature = 'sec-browsing-topics-false', third_party_count, 0)) AS sec_browsing_topics_false,
+  SUM(IF(feature = 'sec-browsing-topics-true', third_party_count, 0)) AS sec_browsing_topics_true,
+  SUM(IF(feature = 'navigator.userAgentData.getHighEntropyValues', third_party_count, 0)) AS navigator_userAgentData_getHighEntropyValues,
+  SUM(IF(feature = 'Sec-CH-DPR', third_party_count, 0)) AS Sec_CH_DPR,
+  SUM(IF(feature = 'Sec-CH-Device-Memory', third_party_count, 0)) AS Sec_CH_Device_Memory,
+  SUM(IF(feature = 'Sec-CH-Downlink', third_party_count, 0)) AS Sec_CH_Downlink,
+  SUM(IF(feature = 'Sec-CH-ECT', third_party_count, 0)) AS Sec_CH_ECT,
+  SUM(IF(feature = 'Sec-CH-Forced-Colors', third_party_count, 0)) AS Sec_CH_Forced_Colors,
+  SUM(IF(feature = 'Sec-CH-Prefers-Color-Scheme', third_party_count, 0)) AS Sec_CH_Prefers_Color_Scheme,
+  SUM(IF(feature = 'Sec-CH-Prefers-Contrast', third_party_count, 0)) AS Sec_CH_Prefers_Contrast,
+  SUM(IF(feature = 'Sec-CH-Prefers-Reduced-Data', third_party_count, 0)) AS Sec_CH_Prefers_Reduced_Data,
+  SUM(IF(feature = 'Sec-CH-Prefers-Reduced-Motion', third_party_count, 0)) AS Sec_CH_Prefers_Reduced_Motion,
+  SUM(IF(feature = 'Sec-CH-Prefers-Reduced-Transparency', third_party_count, 0)) AS Sec_CH_Prefers_Reduced_Transparency,
+  SUM(IF(feature = 'Sec-CH-RTT', third_party_count, 0)) AS Sec_CH_RTT,
+  SUM(IF(feature = 'Sec-CH-Save-Data', third_party_count, 0)) AS Sec_CH_Save_Data,
+  SUM(IF(feature = 'Sec-CH-UA', third_party_count, 0)) AS Sec_CH_UA,
+  SUM(IF(feature = 'Sec-CH-UA-Arch', third_party_count, 0)) AS Sec_CH_UA_Arch,
+  SUM(IF(feature = 'Sec-CH-UA-Architecture', third_party_count, 0)) AS Sec_CH_UA_Architecture,
+  SUM(IF(feature = 'Sec-CH-UA-Bitness', third_party_count, 0)) AS Sec_CH_UA_Bitness,
+  SUM(IF(feature = 'Sec-CH-UA-Form-Factor', third_party_count, 0)) AS Sec_CH_UA_Form_Factor,
+  SUM(IF(feature = 'Sec-CH-UA-Form-Factors', third_party_count, 0)) AS Sec_CH_UA_Form_Factors,
+  SUM(IF(feature = 'Sec-CH-UA-Full', third_party_count, 0)) AS Sec_CH_UA_Full,
+  SUM(IF(feature = 'Sec-CH-UA-Full-Version', third_party_count, 0)) AS Sec_CH_UA_Full_Version,
+  SUM(IF(feature = 'Sec-CH-UA-Full-Version-List', third_party_count, 0)) AS Sec_CH_UA_Full_Version_List,
+  SUM(IF(feature = 'Sec-CH-UA-Mobile', third_party_count, 0)) AS Sec_CH_UA_Mobile,
+  SUM(IF(feature = 'Sec-CH-UA-Model', third_party_count, 0)) AS Sec_CH_UA_Model,
+  SUM(IF(feature = 'Sec-CH-UA-Platform', third_party_count, 0)) AS Sec_CH_UA_Platform,
+  SUM(IF(feature = 'Sec-CH-UA-Platform-Version', third_party_count, 0)) AS Sec_CH_UA_Platform_Version,
+  SUM(IF(feature = 'Sec-CH-UA-Platform-WoW64', third_party_count, 0)) AS Sec_CH_UA_Platform_WoW64,
+  SUM(IF(feature = 'Sec-CH-UA-Platformuser-Agent', third_party_count, 0)) AS Sec_CH_UA_Platformuser_Agent,
+  SUM(IF(feature = 'Sec-CH-UA-UA', third_party_count, 0)) AS Sec_CH_UA_UA,
+  SUM(IF(feature = 'Sec-CH-UA-WoW64', third_party_count, 0)) AS Sec_CH_UA_WoW64,
+  SUM(IF(feature = 'Sec-CH-Viewport-Height', third_party_count, 0)) AS Sec_CH_Viewport_Height,
+  SUM(IF(feature = 'Sec-CH-Viewport-Width', third_party_count, 0)) AS Sec_CH_Viewport_Width,
+  SUM(IF(feature = 'Sec-CH-Width', third_party_count, 0)) AS Sec_CH_Width
+FROM grouped_features
+GROUP BY publisher
+ORDER BY publisher;
